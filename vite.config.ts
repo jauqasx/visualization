737980b7eb812ea1205f03a028c38ee77444b1ca
liujunqi147 <112102017+jauqasx@users.noreply.github.com>
2023-05-01 03:16:12 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  base: "./",
+  base: "/visualization/",
   publicDir: "public",
   resolve: {
     alias: {
