edbf5fc6600bf9e3155ad7617840d8ed5f0169b9
liujunqi147 <112102017+jauqasx@users.noreply.github.com>
2023-05-01 02:18:59 +0800

    打包修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       name: "首页",
