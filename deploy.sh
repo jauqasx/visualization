22cec857197d924181010bf5ad8f2796611b9b7c
liujunqi147 <112102017+jauqasx@users.noreply.github.com>
2023-05-01 03:41:07 +0800

    打包文件

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,18 @@
-# 发生任何错误时终止
+# 当发生错误时中止脚本
 set -e
 
 # 构建
 npm run build
 
-# 进入输出产物文件夹
+# cd 到构建输出的目录下 
 cd dist
-
-# 如果你要部署到自定义域名
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果你要部署在 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Wang9977/viteProject.git master:gh-pages
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:jauqasx/visualization.git master:gh-pages
 
 cd -
+
